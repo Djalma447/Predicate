ebdeca8fd721659d0884727f740bcfe827bf8c8b
Djalma <djalma.ant@gmail.com>
2020-05-04 01:06:49 -0300

    Reference Method

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
 
 namespace Course
 {
@@ -6,7 +8,23 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest); // Usando o Método 'ProductTest' como referência para realizar função desejada
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0; // Método Auxiliar para ser usado como Argumento para o Método 'RemoveAll'
         }
     }
 }
